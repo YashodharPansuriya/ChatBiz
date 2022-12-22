a31f0637cd732d256a00f8d2858494ea0be37b89
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2022-12-22 13:37:28 +0100

    Send user message to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
 const socket = io();
+const  chatForm = document.getElementById('chat-form'); 
 
+//Messages from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
+
+//Message submit
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //Get message text
+    const msg = e.target.elements.msg.value;
+
+    //Emit message to server
+    socket.emit('chatMessage', msg);
+});
+
+//output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Amit <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
